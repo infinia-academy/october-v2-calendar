2cdd58b4c1d36eeec41dfd22357062a84495f0f2
infinia-academy <infinia.ac@gmail.com>
2026-09-18 16:43:53 +0700

    Fix V2 timezone display and standardize science codes

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='october-v2-cache-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='oct-v2-not5-datefix-v4',A=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
